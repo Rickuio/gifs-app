acd7bfde29d96dd317002703cb544c998a5c13e8
angel_anchaluisa <angel_anchaluisa@trabajo.gob.ec>
2025-05-06 10:35:31 -0500

    Consumo de images desde giphy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@ export const environment = {
     companyName: 'Gifs',
     companyName2: 'App',
     companySlogan: 'Maneja tus gifs',
+    
     //Api Keys
+    giphyApiKey: 'H4JTWakhMGBaEgWKVsIVcy3u9RIYmHS0',
+    giphyUrl: 'https://api.giphy.com/v1/'
 
     //Urls
 };
